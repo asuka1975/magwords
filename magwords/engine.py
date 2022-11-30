d8c3c2ec5c09e9e503868a8eed8e9915018181d8
asuka1975 <39215088+asuka1975@users.noreply.github.com>
2022-11-30 17:28:17 +0900

    feat: designed interface of `MagWordsEngine`, text-rendering engine * MagWordsEngine   add_text_block: add printed characters   compile: convert all added characters to drawable format   draw: draw all added characters

---
+++ magwords/engine.py
@@ -0,0 +1,12 @@
+class MagWordsEngine:
+    def __init__(self):
+        pass
+
+    def add_text_block(self, text_block):
+        pass
+
+    def compile(self):
+        pass
+
+    def draw(self):
+        pass
