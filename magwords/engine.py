2b184fa86a09ec7df79eff36efc6f69bef5de680
asuka1975 <shikina.asuka@i.mbox.nagoya-u.ac.jp>
2022-12-15 15:36:03 +0900

    feat: Remove add_textblock and compile methods from MagwordsEngine. The add_textblock method was deleted because the TextBlock class was deleted. Deleted the compile method because the OpenGL buffer and shaders are generated by the __init__ method, and the character transforms are transferred to the OpenGL buffer by a high-level API to be added in the future.

--- magwords/engine.py
+++ magwords/engine.py
@@ -2,11 +2,5 @@ class MagWordsEngine:
     def __init__(self):
         pass
 
-    def add_text_block(self, text_block):
-        pass
-
-    def compile(self):
-        pass
-
     def draw(self):
         pass
