7acf7e6e653366b57a31b4f28c0a2d8ed3b7c733
asuka1975 <39215088+asuka1975@users.noreply.github.com>
2022-12-19 02:09:14 +0900

    fix: Modified the typo `dictionary` to `dictionary`.

--- magwords/platform/path/linux.py
+++ magwords/platform/path/linux.py
@@ -4,9 +4,9 @@ import subprocess
 from .fontfamily import createFontFamilyDict
 
 font_directories: list[str] = [os.path.join(d, "fonts") for d in os.environ.get("XDG_DATA_DIRS").split(":")]
-font_dicationary: dict[str, dict[str, str]] = createFontFamilyDict(font_directories)
+font_dictionary: dict[str, dict[str, str]] = createFontFamilyDict(font_directories)
 
 try:
-    default_font: str = font_dicationary[subprocess.run(["fc-match", "-f", "%{family}"], capture_output=True, text=True).stdout][subprocess.run(["fc-match", "-f", "%{style}"], capture_output=True, text=True).stdout]
+    default_font: str = font_dictionary[subprocess.run(["fc-match", "-f", "%{family}"], capture_output=True, text=True).stdout][subprocess.run(["fc-match", "-f", "%{style}"], capture_output=True, text=True).stdout]
 except FileNotFoundError:
     raise NotImplementedError()
