81b0053ae4a3a809b53659a4e464d919dcea1dc6
asuka1975 <shikina.asuka@i.mbox.nagoya-u.ac.jp>
2022-12-19 02:17:35 +0900

    update: Change default_font from font path to family and style pair

--- magwords/platform/path/linux.py
+++ magwords/platform/path/linux.py
@@ -7,6 +7,6 @@ font_directories: list[str] = [os.path.join(d, "fonts") for d in os.environ.get(
 font_dictionary: dict[str, dict[str, str]] = createFontFamilyDict(font_directories)
 
 try:
-    default_font: str = font_dictionary[subprocess.run(["fc-match", "-f", "%{family}"], capture_output=True, text=True).stdout][subprocess.run(["fc-match", "-f", "%{style}"], capture_output=True, text=True).stdout]
+    default_font: tuple[str, str] = (subprocess.run(["fc-match", "-f", "%{family}"], capture_output=True, text=True).stdout, subprocess.run(["fc-match", "-f", "%{style}"], capture_output=True, text=True).stdout)
 except FileNotFoundError:
     raise NotImplementedError()
--- magwords/platform/path/windows.py
+++ magwords/platform/path/windows.py
@@ -6,6 +6,6 @@ font_directories: list[str] = [os.path.join(os.environ.get("WINDIR"), "Fonts")]
 font_dictionary: dict[str, dict[str, str]] = createFontFamilyDict(font_directories)
 
 if platform.win32_ver()[0] == "10":
-    default_font: str = font_dictionary["Yu Gothic"]["Regular"]
+    default_font: tuple[str, str] = ("Yu Gothic", "Regular")
 else:
     raise NotImplementedError()
