fc0002e69bc6984c98a30cca1affb554f3e21d06
asuka1975 <shikina.asuka@i.mbox.nagoya-u.ac.jp>
2022-12-19 02:09:14 +0900

    fix: Modified the typo `dictionary` to `dictionary`.

--- magwords/platform/path/windows.py
+++ magwords/platform/path/windows.py
@@ -3,9 +3,9 @@ import platform
 from .fontfamily import createFontFamilyDict
 
 font_directories: list[str] = [os.path.join(os.environ.get("WINDIR"), "Fonts")]
-font_dicationary: dict[str, dict[str, str]] = createFontFamilyDict(font_directories)
+font_dictionary: dict[str, dict[str, str]] = createFontFamilyDict(font_directories)
 
 if platform.win32_ver()[0] == "10":
-    default_font: str = font_dicationary["Yu Gothic"]["Regular"]
+    default_font: str = font_dictionary["Yu Gothic"]["Regular"]
 else:
     raise NotImplementedError()
