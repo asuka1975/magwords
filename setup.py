054b42568474da0e3c0c58091481ee63d5c1317b
asuka1975 <39215088+asuka1975@users.noreply.github.com>
2022-12-20 15:34:44 +0900

    feat: make `magwords` installable

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 from codecs import open
 from os import path
 
-with open(path.join(path.abspath(path.dirname(__file__))), encoding="utf-8") as f:
+with open(path.join(path.abspath(path.dirname(__file__)), "README.md"), encoding="utf-8") as f:
     description = f.read()
 
 setup(
     name="magwords",
-    packages=[],
+    packages=find_packages(),
+    package_data={"magwords" : ["shaders/*"]},
     version="1.0.0",
     license="MIT",
     install_requires=["cu2qu", "freetype-py", "glfw", "numpy", "PyGLM", "PyOpenGL", "scipy"],
