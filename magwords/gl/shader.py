d62e0ea5fdd7b0efcd2c4a0762d3e012b5b05079
asuka1975 <shikina.asuka@i.mbox.nagoya-u.ac.jp>
2023-01-07 17:39:12 +0900

    feat: Define a class that creates a Shader object

--- magwords/gl/shader.py
+++ magwords/gl/shader.py
@@ -1,42 +1,20 @@
 import sys
 from OpenGL.GL import *
 
-class ShaderProgram:
-    def __init__(self):
-        self.handle = glCreateProgram()
-    
-    def __del__(self):
-        if self.handle is not None:
-            glDeleteProgram(self.handle)
+class Shader:
+    def __init__(self, content, type_):
+        self.handle = glCreateShader(type_)
+        glShaderSource(self.handle, [content])
+        glCompileShader(self.handle)
+
+        status = ctypes.c_uint()
+        glGetShaderiv(self.handle, GL_COMPILE_STATUS, status)
+        if not status:
+            print(glGetShaderInfoLog(self.handle).decode("utf-8"), file=sys.stderr)
+            glDeleteShader(self.handle)
             self.handle = None
 
-    def attach_shader(self, content, type, log_always=False):
-        shader = glCreateShader(type)
-        glShaderSource(shader, [content])
-        glCompileShader(shader)
-
-        status = ctypes.c_uint(GL_UNSIGNED_INT)
-        glGetShaderiv(shader, GL_COMPILE_STATUS, status)
-        if log_always or not status:
-            print(glGetShaderInfoLog(shader).decode("utf-8"), file=sys.stderr)
-            glDeleteShader(shader)
-            return False
-        
-        glAttachShader(self.handle, shader)
-        glDeleteShader(shader)
-        return True
-
-    def link(self, log_always=False):
-        glLinkProgram(self.handle)
-        status = ctypes.c_uint(GL_UNSIGNED_INT)
-        glGetProgramiv(self.handle, GL_LINK_STATUS, status)
-        if log_always or not status:
-            print(glGetProgramInfoLog(self.handle).decode("utf-8"), file=sys.stderr)
-            return False
-        return True
-    
-    def use(self):
-        glUseProgram(self.handle)
-
-    def unuse(self):
-        glUseProgram(0)
+    def __del__(self):
+        if hasattr(self, "handle") and self.handle is not None:
+            glDeleteShader(self.handle)
+            self.handle = None
